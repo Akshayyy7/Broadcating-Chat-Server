eef39bfb37a2897927c10789a2ed30f19bd5d744
Akshayyy7 <33392922+Akshayyy7@users.noreply.github.com>
2018-06-08 18:15:06 +0530

    Update home.php

--- home.php
+++ home.php
@@ -1,22 +1,43 @@
+<?php
+session_start();
+include 'dbh.php';
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
 
 	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="home.css">
 	</head>
 	<body>
 	<div id="main">
 
-	<h1 style="background-color: #6495ed;color: white;">-online</h1>
+	<h1 style="background-color: #6495ed;color: white;"><?php echo 
+	$_SESSION['name'] ?>-online</h1>
 	<div class="output">
+      <?php  $sql= "SELECT   * FROM posts";
+ $result = $conn->query($sql);
 
+ if($result->num_rows > 0){
+ 	 while($row = $result->fetch_assoc()){
+ 	 	echo "" . $row["name"]. " " .":: " . $row["msg"]."-------------------------Time and date----------------------"
+ 	 	.$row["date"].  "<br>";
+ 	 	echo "<br>";
+ 	 }
+ }else{
+ 	echo "0 results";
+ }
+  $conn->close();
+  ?>
 
-
+ 
 	</div>
 
 	<form method="post" action="send.php">
-	<textarea name="msg" placeholder="Type to send message.." class="form-control"></textarea><br>
-	<input type="submit" name="">
+	<textarea name="msg" placeholder="Type your message here" class="form-control"></textarea><br>
+	<input type="submit" value="Send">
 
 
 	</form>
@@ -30,4 +51,3 @@
 	</div>
 	</body>
 	</html>
-
